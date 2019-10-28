f37c09bfe18b5e37a56dad35f7014892c0640db8
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-10-28 10:00:48 -0500

    Init commit. Created visual studio document, created Game class with member variables.

---
+++ LemonadeStand/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Game
+    {
+        //member variables (HAS A)
+        private Player player;
+        private List<int> days;
+        private int currentDay
+
+
+        
+
+
+        //constructor (BUILD)
+
+
+
+        //member methods (CAN DO)
+
+    }
+}
