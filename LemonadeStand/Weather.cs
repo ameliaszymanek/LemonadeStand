001ac49770379d966e5cefeb1bc37b9f8f982829
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-10-28 11:25:31 -0500

    Created Day and Weather class. Working on member methods for Weather class.

---
+++ LemonadeStand/Weather.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Weather
+    {
+        //member variables (HAS A)
+        public string condition;
+        public int temperature;
+        public List<string> weatherConditions;
+
+        //constructor (BUILD)
+        public Weather()
+        {
+            weatherConditions = new List<string>() {"sunny", "cloudy", "rainy"};
+        }
+
+        //member methods (CAN DO)
+
+
+    }
+}
